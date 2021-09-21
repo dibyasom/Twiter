c21c04b9771d1ae163126ea8574f516f86b24d28
vhaegar1526 <dibya.secret@gmail.com>
2021-09-21 07:28:05 +0530

    API views now use generics and mixins.

--- twitter/tweet/urls.py
+++ twitter/tweet/urls.py
@@ -1,7 +1,11 @@
 from . import views
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', views.tweets, name='feed'),
-    path('<int:pk>', views.tweet, name='tweet-details')
+    path('', views.TweetList.as_view(), name='feed'),
+    path('<int:pk>', views.TweetDetail.as_view(), name='tweet-details')
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+print(urlpatterns)
--- twitter/tweet/views.py
+++ twitter/tweet/views.py
@@ -1,68 +1,93 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
-from django.http import HttpRequest, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-
-from rest_framework import status
-
-from rest_framework.parsers import JSONParser
+# drf request, response
+from rest_framework import generics
 
 from tweet.models import Tweet
 from tweet.serializers import TweetSerializer
 
-# Create your views here.
+# DRF mixins
+from rest_framework import generics
 
 
-@csrf_exempt
-def tweets(request: HttpRequest) -> HttpResponse:
+# All tweets.
+class TweetList(generics.ListCreateAPIView):
     '''
-    List all tweets from DB.
+        List all tweets or create new tweet.
     '''
-    if request.method == 'GET':
-        tweets = Tweet.objects.all()
-        serializer = TweetSerializer(tweets, many=True)
-        return JsonResponse(serializer.data, safe=False, status=200)
+    queryset = Tweet.objects.all()
+    serializer_class = TweetSerializer
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = TweetSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+# TODO: Implement PATCH to manipulate a single field of tweet.
 
 
-@csrf_exempt
-def tweet(request: HttpRequest, pk: int) -> HttpResponse:
+class TweetDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a tweet.
+    """
+    queryset = Tweet.objects.all()
+    serializer_class = TweetSerializer
 
-    try:
-        # Fetch the tweek.
-        tweet = Tweet.objects.get(pk=pk)
-    except Tweet.DoesNotExist:
-        return HttpResponse(status=404)
+# ####################################################### #
+# MANUAL WAY, More control. w/o using generics or mixins. #
+# ####################################################### #
 
-    # Return Tweet details.
-    if request.method == 'GET':
-        # Serialize and return JSON res.
-        serializer = TweetSerializer(tweet)
-        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+# # All tweets.
+# class TweetList(APIView):
+#     '''
+#         List all tweets or create new tweet.
+#     '''
 
-    # Delete tweet.
-    if request.method == 'DELETE':
-        tweet.delete()
-        return HttpResponse(status=204)
+#     # List all tweets
+#     def get(self, request: Request, format=None):
+#         tweets = Tweet.objects.all()
+#         serializer = TweetSerializer(tweets, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    # Update/Edit tweet.
-    if request.method == 'PUT':
-        # Convert to native python datatype.
-        data = JSONParser().parse(request)
-        
-        # Deserialize
-        serializer = TweetSerializer(tweet, data=data)
+#     # Create new tweet.
+#     def post(self, request: Request, format=None):
+#         serializer = TweetSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+# # TODO: Implement PATCH to manipulate a single field of tweet.
 
-    return HttpResponse(status=404)
+# class TweetDetail(APIView):
+#     """
+#     Retrieve, update or delete a tweet.
+#     """
+
+#     def get_tweet(self, pk):
+#         try:
+#             # Fetch the tweek.
+#             return Tweet.objects.get(pk=pk)
+#         except Tweet.DoesNotExist:
+#             raise Response(status=status.HTTP_404_NOT_FOUND)
+
+#     # Return Tweet details.
+#     def get(self, request: Request, pk: int, format=None):
+#         tweet = self.get_tweet(pk=pk)
+
+#         # Serialize and return JSON res.
+#         serializer = TweetSerializer(tweet)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+#     # Update/Edit tweet.
+
+#     def put(self, request: Request, pk: int, format=None):
+#         tweet = self.get_tweet(pk=pk)
+
+#         # Deserialize
+#         serializer = TweetSerializer(tweet, data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     # Delete tweet.
+#     def delete(self, request: Request, pk: int, format=None):
+#         tweet = self.get_tweet(pk=pk)
+
+#         tweet.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
